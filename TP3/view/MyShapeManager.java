79a7b2a88d0d74a51316db658a87dcd5678012fd
Valentin Allmnd <121107522+AsthaFinito@users.noreply.github.com>
2023-05-02 18:02:44 +0200

    Test Ajout de la seconde vue

--- TP3/view/MyShapeManager.java
+++ TP3/view/MyShapeManager.java
@@ -1,60 +1,61 @@
 package org.isen.volumeModel.TP3.view;
 
-import org.isen.volumeModel.TP3.Data.Shape;
-import org.isen.volumeModel.TP3.model.ShapeListModel;
 
-import javax.swing.*;
+
+import org.isen.volumeModel.TP3.controller.DrawController;
+import org.isen.volumeModel.TP3.model.ShapeListModel;
+import org.isen.volumeModel.TP3.view.IDrawView;
+import org.isen.volumeModel.TP3.Data.Shape;
+import org.isen.volumeModel.TP3.view.impl.Dessin;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
 import java.util.List;
-
+import javax.swing.*;
+import javax.swing.plaf.synth.SynthTextAreaUI;
 public class MyShapeManager extends JPanel implements ActionListener,IDrawView {
 
     private final ShapeListModel shapeListModel;
-    private final JList<String> shapeList;
-    private final JButton removeButton;
+    //private final JList<String> shapeList;
+  //  private final JButton removeButton;
+    private final DrawController controller;
+    private final Dessin dessin;
 
-    public MyShapeManager(ShapeListModel shapeListModel) {
+    public MyShapeManager(String title,DrawController controller,ShapeListModel shapeListModel) {
+        super(Boolean.parseBoolean(title));
         this.shapeListModel = shapeListModel;
 
+
+
+        this.controller=controller;
+        this.dessin = new Dessin(this.controller);
+        this.controller.RegisterView(this);
+        setLocation(0,0);
+        setSize(1280,760);
+
+       // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Création de la liste de formes
-        shapeList = new JList<>();
-        shapeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        JScrollPane scrollPane = new JScrollPane(shapeList);
-        add(scrollPane);
+       // shapeList = new JList<>();
+        //shapeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        //JScrollPane scrollPane = new JScrollPane(shapeList);
+        //add(scrollPane);
 
         // Création du bouton remove
-        removeButton = new JButton("Remove");
+        JButton removeButton = new JButton("Remove");
         removeButton.addActionListener(this);
         add(removeButton);
     }
 
     // Met à jour la liste de formes affichée
-    public void updateShapeList() {
-        List<org.isen.volumeModel.TP3.Data.Shape> shapes = shapeListModel.getShapes();
-        DefaultListModel<String> model = new DefaultListModel<>();
-        for (Shape shape : shapes) {
-            model.addElement(shape.getClass().getSimpleName());
-        }
-        shapeList.setModel(model);
-    }
 
-    // Réagit au clic sur le bouton remove
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == removeButton) {
-            int index = shapeList.getSelectedIndex();
-            if (index >= 0) {
-               // shapeListModel.RemoveShape(index);
-            }
-        }
-    }
 
     @Override
     public void display() {
         setVisible(true);
+        System.out.println("Affichage de le deuxième fenetre? ");
     }
 
     @Override
@@ -64,6 +65,11 @@ public class MyShapeManager extends JPanel implements ActionListener,IDrawView {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
